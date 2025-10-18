e18a327c750d95fc77b77c1c702167d996e15a8a
Cesar Trejo <156372979+ctdavila@users.noreply.github.com>
2025-10-17 21:37:40 -0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,6 @@ def driver():
     """Inicia Chrome con Selenium Manager y cierra al final del test."""
     options = Options()
     options.add_argument("--start-maximized")
-    # Si tu pantalla es chica: options.add_argument("--window-size=1280,900")
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
